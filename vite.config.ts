63fd1d171288750e83454cf502dc436bec35e0d5
Yonatan Kaminer <yonatankaminer@gmail.com>
2026-02-05 01:47:44 +0200

    wip: end of day

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      "k8s-button0-button0f-e1297085bf-1621650183.eu-central-1.elb.amazonaws.com",
+    ],
+  },
+});
+
